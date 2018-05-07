486764a9ee88a98b1a2bae7b841b3008abd1f4c3
xxmuaddib <xxmuaddib@gmail.com>
2018-05-07 10:28:03 +0400

    Whitelist implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,11 @@ export class AppComponent {
   accounts: any;
 
   balance: number;
-  sendingAmount: number;
-  recipientAddress: string;
+  contractBalance: number;
+  whitelistAmount: number;
+  ethAddressAdd: string;
+  ethAddressRemove: string;
+  ethAddressCheck: string;
   status: string;
   canBeNumber = canBeNumber;
 
@@ -40,9 +43,21 @@ export class AppComponent {
       // This is run from window:load and ZoneJS is not aware of it we
       // need to use _ngZone.run() so that the UI updates on promise resolution
       this._ngZone.run(() =>
-        this.refreshBalance()
+        this.runInitialCalls()
       );
     }, err => alert(err))
+  };
+
+  runInitialCalls = () => {
+    this.refreshBalance();
+    this.getContractBalance();
+  }
+
+  getContractBalance = () => {
+    this.svandisSaleService.getContractBalance(this.account)
+      .subscribe(value => {
+        this.contractBalance = value
+      }, e => {this.setStatus('Error getting contract balance; see log.')})
   };
 
   refreshBalance = () => {
@@ -56,13 +71,33 @@ export class AppComponent {
     this.status = message;
   };
 
-  sendCoin = () => {
+  addToWhitelist = () => {
     this.setStatus('Initiating transaction... (please wait)');
 
-    this.svandisSaleService.sendCoin(this.account, this.recipientAddress, this.sendingAmount)
+    this.svandisSaleService.addToWhitelist(this.ethAddressAdd, this.whitelistAmount, this.account)
       .subscribe(() =>{
-        this.setStatus('Transaction complete!');
+        this.setStatus('Address added to whitelist with '+this.whitelistAmount+' amount');
         this.refreshBalance();
-      }, e => this.setStatus('Error sending coin; see log.'))
+      }, e => this.setStatus('Error adding to whitelist; see log.'))
+  };
+
+  removeFromWhitelist = () => {
+    this.setStatus('Initiating transaction... (please wait)');
+
+    this.svandisSaleService.removeFromWhitelist(this.ethAddressRemove, this.account)
+      .subscribe(() =>{
+        this.setStatus('Address removed from whitelist!');
+        this.refreshBalance();
+      }, e => this.setStatus('Error removing from whitelist; see log.'))
+  };
+
+  checkWhitelisted = () => {
+    this.setStatus('Initiating transaction... (please wait)');
+
+    this.svandisSaleService.checkWhitelisted(this.ethAddressCheck, this.account)
+      .subscribe((d) =>{
+        this.setStatus('Account whitelist amount is ' + d.toNumber());
+        this.refreshBalance();
+      }, e => this.setStatus('Error checking whitelist; see log.'))
   };
 }
--- src/services/svandis-sale.service.ts
+++ src/services/svandis-sale.service.ts
@@ -18,6 +18,31 @@ export class SvandisSaleService {
   	this.Sale.setProvider(web3Ser.web3.currentProvider);
   }
 
+  getContractBalance(account): Observable<number> {
+    let meta;
+
+    return Observable.create(observer => {
+      this.Sale
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          console.log(meta.address);
+          //we use call here so the call doesn't try and write, making it free
+          return meta.balanceOf.call(meta.address, {
+            from: account
+          });
+        })
+        .then(value => {
+          observer.next(value)
+          observer.complete()
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e)
+        });
+    })
+  }
+
   getBalance(account): Observable<number> {
   	let meta;
 
@@ -26,13 +51,13 @@ export class SvandisSaleService {
   		  .deployed()
   		  .then(instance => {
   		    meta = instance;
+          console.log(meta);
           //we use call here so the call doesn't try and write, making it free
   		    return meta.balanceOf.call(account, {
   		      from: account
   		    });
   		  })
   		  .then(value => {
-          console.log(value);
   		    observer.next(value)
   		    observer.complete()
   		  })
@@ -43,7 +68,7 @@ export class SvandisSaleService {
   	})
   }
 
-  sendCoin(from, to, amount): Observable<any>{
+  addToWhitelist(ethAddress, amount, account): Observable<any>{
 
   	let meta;
   	return Observable.create(observer => {
@@ -51,19 +76,65 @@ export class SvandisSaleService {
   	    .deployed()
   	    .then(instance => {
   	      meta = instance;
-  	      return meta.sendCoin(to, amount, {
-  	        from: from
-  	      });
+  	      return meta.addToWhitelist(ethAddress, amount, {
+            from: account
+          });
   	    })
   	    .then(() => {
   	      observer.next()
-  	      observer.next()
+          observer.complete()
   	    })
   	    .catch(e => {
   	    	console.log(e);
   	      observer.error(e)
   	    });
   	})
+  }
+
+  removeFromWhitelist(ethAddress, account): Observable<any>{
+
+    let meta;
+    return Observable.create(observer => {
+      this.Sale
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          return meta.removeFromWhitelist(ethAddress, {
+            from: account
+          });
+        })
+        .then(() => {
+          observer.next()
+          observer.complete()
+        })
+        .catch(e => {
+          console.log(e);
+          observer.error(e)
+        });
+    })
+  }
+
+  checkWhitelisted(ethAddress, account): Observable<any>{
+
+    let meta;
+    return Observable.create(observer => {
+      this.Sale
+        .deployed()
+        .then(instance => {
+          meta = instance;
+          return meta.checkWhitelisted.call(ethAddress, {
+            from: account
+          });
+        })
+        .then((value) => {
+          observer.next(value)
+          observer.complete()
+        })
+        .catch(e => {
+          console.log(e.ToNumber());
+          observer.error(e)
+        });
+    })
   }
 
 }
