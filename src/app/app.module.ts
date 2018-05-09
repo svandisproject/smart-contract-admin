bf4537ed6c27bcdd5187886f81e4cccba70e1253
xxmuaddib <xxmuaddib@gmail.com>
2018-05-09 18:37:06 +0400

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './routes/main/main.component';
+import { RouterModule, Routes } from '@angular/router';
 import {SvandisSaleService, Web3Service} from '../services/services'
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'home', component: MainComponent }
+];
 
 const SERVICES = [
   SvandisSaleService,
@@ -13,12 +22,17 @@ const SERVICES = [
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(
+      appRoutes, 
+      { enableTracing: true }
+    ),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   providers: [SERVICES],
   bootstrap: [AppComponent]
