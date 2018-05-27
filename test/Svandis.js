c83907517e87c1e5495b0f3158ebc22fb43e289c
victorwiebe <vw@akropolis.io>
2018-05-27 15:41:15 -0400

    Add unit tests to cover on top of the Svandis contract

--- test/Svandis.js
+++ test/Svandis.js
@@ -10,7 +10,7 @@ const should = require('chai')
 	.use(require('chai-bignumber')(BigNumber))
 	.should();
 
-contract('Svandis Token', function () {
+contract('Svandis Token', function ([holderAddress, recipientAddress, anyAddress]) {
 
 	let token;
 
@@ -25,5 +25,32 @@ contract('Svandis Token', function () {
 		(await token.symbol()).should.be.equal("SVN");
 		(await token.version()).should.be.equal("SVN 1.0");
 	});
+
+	it('should not transfer token value without a balance', async function() {
+		await token.transfer(recipientAddress, 1, {from: holderAddress}).should.be.rejectedWith('revert');
+	});
+
+
+	it('should not transfer from approved token value without a balance and without approval', async function() {
+		await token.transferFrom(holderAddress, recipientAddress, 1, {from: holderAddress}).should.be.rejectedWith('revert');
+	});
+
+
+	it('should return a balance of 0 for arbitrary holder address before a sale', async function() {
+		(await token.balanceOf(holderAddress, {from: recipientAddress})).should.be.bignumber.equal(0);
+	});
+
+
+	it('should approve a token to be moved from the message sender to a recipient address', async function() {
+		await token.approve(recipientAddress, 1, {from: holderAddress}).should.be.fulfilled;
+		(await token.allowance(holderAddress, recipientAddress, {from: anyAddress})).should.be.bignumber.equal(1);
+	});
+
+
+	it('should approve ambiguous number of tokens to be alternatively moved from the recipient sender to the holder address', async function() {
+		await token.approve(holderAddress, 100, {from: recipientAddress}).should.be.fulfilled;
+		(await token.allowance(recipientAddress, holderAddress, {from: anyAddress})).should.be.bignumber.equal(100);
+	});
+
 
 });
