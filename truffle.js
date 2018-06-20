1511aa05bdfb96d6e846757634dd36fe49ed60b7
victorwiebe <vw@akropolis.io>
2018-06-19 21:54:20 -0400

    Configure the project to use kovan network, add mnenomic and ethereum node via environment variables

--- truffle.js
+++ truffle.js
@@ -1,12 +1,22 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 
+const HDWalletProvider = require("./tools/svandis-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    kovan: {
+      provider: function() {
+            return new HDWalletProvider(process.env.mnemonic, process.env.ethereumnode, 0, 3)
+      },
+      network_id: 42,
+      gas: 6000000, // High gas to get it through
+      gasPrice: 0x05
     }
   }
 }
